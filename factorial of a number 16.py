83cc24fe70de9d8e13491ad39324178df64aadde
Aariya pandey <119661488+Aariya-Pandey-22@users.noreply.github.com>
2022-12-02 14:41:49 +0530

    Add files via upload

---
+++ factorial of a number 16.py
@@ -0,0 +1,19 @@
+# n=int(input("enter number"))
+# fac=1
+# while n>0:
+#     fac=fac*n
+#     n=n-1
+#     print(fac)
+# print("fectorial=",fac) 
+
+
+
+
+i=1
+sum=0
+while i<=5:
+    n=int(input("enter the number"))
+    sum=sum+n
+    print(sum)
+    i=i+1
+print("total",sum)
